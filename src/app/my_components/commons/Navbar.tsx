6e11721881af6f0a85b15e4fd47ea692f16fe291
sutesumit <sumitsute9@gmail.com>
2025-02-08 17:39:27 +0530

    nav elements centered

--- src/app/my_components/commons/Navbar.tsx
+++ src/app/my_components/commons/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav className='absolute top-12 left-[50vw] -translate-x-[50%] px-2 py-1 rounded-md bg-[var(--primary-blue)] font-body text-xs cursor-pointer'>
-        <ul className='flex m-auto justify-center gap-1 opacity-75'>
+        <ul className='flex m-auto justify-center items-center gap-1 opacity-75'>
             <Link href="/"><li className='nav-btn'>Home</li></Link>
             <Link href="/facilitator"><li className='nav-btn'>Facilitator</li></Link>
             <Link href="/resources"><li className='nav-btn'>Resources</li></Link>
