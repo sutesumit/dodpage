4b019fef02d2de8050cca87466c4a107465e42b8
sutesumit <sumitsute9@gmail.com>
2025-02-11 01:17:09 +0530

    gallery video added

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 import React from 'react'
-import { Slab }  from 'react-loading-indicators'
 
 const Home = () => {
   return (
-    <div className='page pt-12 flex flex-col justify-center items-center w-full h-full rounded-md bg-[var(--primary-white)] border-[var(--primary-blue)] border-[0.25px] text-[var(--primary-blue)]'>
-            <Slab color="#131857" size="medium" text="" textColor="" />
-            <p className='text-center font-body text-xs max-w-[80ch] p-2 opacity-75'>Hello, Gallery Loading!</p>
+    <div className='page flex flex-col justify-center items-center w-full h-full rounded-md bg-[var(--primary-white)] border-[var(--primary-blue)] border-[0.25px] text-[var(--primary-blue)]'>
+      <div className="aspect-w-16 aspect-h-9 w-full h-full overflow-clip">
+        <iframe
+          className="w-full h-full"
+          src={`https://www.youtube.com/embed/_H9tByQyBv8?loop=1&playlist=_H9tByQyBv8&modestbranding=1&rel=0&iv_load_policy=3&cc_load_policy=0`}
+          title="YouTube Video"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
     </div>
   )
 }
