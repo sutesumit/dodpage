55df5d29a5d623a4a5cc83e788ee3f89c608f775
sutesumit <sumitsute9@gmail.com>
2025-02-12 15:19:08 +0530

    resource page size fixed

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -7,26 +7,28 @@ const Home = () => {
   return (
     <div className="page flex flex-col items-center justify-center bg-[var(--primary-blue)] w-full h-full rounded-md">
       
-      <div className='max-h-[70vh] object-contain flex w-[60vw] overflow-hidden'>
+      <div className='object-contain flex h-[70vh] min-w-[214px] md:w-[70%] overflow-hidden p-2'>
         {// @ts-expect-error too many props
         <HTMLFlipBook
           width={70}
           height={100}
           size="stretch"
           style = {{width: '100%', height: '100%'}}
+          usePortrait={true}
+
         >
-          {Array.from({ length: 25}, (_, index)  => (
+          {Array.from({length: 25}, (_, index)  => (
             <div key={index} className="page">
               <Image 
                 src={`/manual/ARC_DoD_Manual_Page-${index}.jpg`} 
-                alt={`Facilitator Manual Page ${index}`} 
+                alt={`Facilitator Manual Page ${index+1}`} 
                 layout='fill'
                 className='border-[0.25px] border-[var(--primary-blue)] rounded-md'
               />
             </div> ))}
         </HTMLFlipBook>}
       </div> 
-      <h5 className='text-xs text-center pt-2 opacity-75 before:content-["-"] after:content-["-"]'>Leaf through the Dramas of Discrimination Workshop Facilitation Manual</h5> 
+      <h5 className='text-xs text-center p-2 opacity-75 before:content-["-"] after:content-["-"]'>Leaf through the Dramas of Discrimination Workshop Facilitation Manual</h5> 
     </div>
   )
 }
