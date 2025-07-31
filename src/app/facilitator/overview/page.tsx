ee7c39d9f79f3ac70e58ce90cd5fe848b74b14a3
sutesumit <sumitsute9@gmail.com>
2025-07-31 12:26:57 +0530

    page(facilitator/overview): initiate the page

---
+++ src/app/facilitator/overview/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex text-[var(--primary-blue)] flex-col justify-center items-center w-full h-full p-5 border-[var(--primary-blue)] border-[0.25px] rounded-md bg-[var(--primary-white)]'>
+      This is Overview page under construction
+    </div>
+  )
+}
+
+export default page
--- src/app/facilitator/page.tsx
+++ src/app/facilitator/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 import FacilitatorLanding from './FacilitatorLanding';
-
+import Link from 'next/link';
 const Home = () => {
   return (
     <>
       <FacilitatorLanding />
-      <div className='absolute bottom-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50'>
-        <button className="bg-[var(--primary-blue)] text-[var(--primary-white)] py-1 px-6 rounded-md">
+      <div className='absolute bottom-[10vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50'>
+        <Link 
+          href="/facilitator/overview" 
+          className="bg-[var(--primary-blue)] text-[var(--primary-white)] py-1 px-6 rounded-md"
+        >
           Begin
-        </button>
+        </Link>
       </div>
     </>
   )
