55ebefb689c541bd58b73362937057a3b9e027df
sutesumit <sumitsute9@gmail.com>
2025-07-31 12:11:05 +0530

    style: update for/of animation to letterSpacing

--- src/app/facilitator/page.tsx
+++ src/app/facilitator/page.tsx
@@ -6,8 +6,8 @@ const Home = () => {
     <>
       <FacilitatorLanding />
       <div className='absolute bottom-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50'>
-        <button className="bg-[var(--primary-blue)] text-[var(--primary-white)] py-1 px-3 rounded-md">
-          Button
+        <button className="bg-[var(--primary-blue)] text-[var(--primary-white)] py-1 px-6 rounded-md">
+          Begin
         </button>
       </div>
     </>
