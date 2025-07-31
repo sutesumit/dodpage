8fa672859e97e8a231bfd7c38da0e576934de7ba
sutesumit <sumitsute9@gmail.com>
2025-07-31 11:11:05 +0530

    style: implement new facilitator landing page with animated title array

--- src/app/facilitator/page.tsx
+++ src/app/facilitator/page.tsx
@@ -1,13 +1,15 @@
 'use client';
-import Landing from '../my_components/elements/Landing';
+import FacilitatorLanding from './FacilitatorLanding';
 
 const Home = () => {
   return (
     <>
-      <Landing colorScheme="light" />
-      <button className="bg-[var(--primary-blue)] text-[var(--primary-white)] py-1 px-3 rounded-md">
-        Button
-      </button>
+      <FacilitatorLanding />
+      <div className='absolute bottom-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50'>
+        <button className="bg-[var(--primary-blue)] text-[var(--primary-white)] py-1 px-3 rounded-md">
+          Button
+        </button>
+      </div>
     </>
   )
 }
