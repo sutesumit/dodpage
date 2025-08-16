b4499357aa2a02afb2c82383195a2d66e0f0000c
sutesumit <sumitsute9@gmail.com>
2025-08-16 21:45:55 +0530

    feat(parttime): build Part B interface with collapsible sections

--- src/app/facilitator/parttime/PartBContent.tsx
+++ src/app/facilitator/parttime/PartBContent.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import PartBDivision from './PartBDivision'
+import PartBSheet from './PartBSheet'
+import PartBInstruction from './PartBInstruction'
 
 const PartBContent = () => {
   return (
-    <div>
-      <p>Part B Content</p>
+    <div className='flex flex-col gap-2 items-center justify-center h-full w-full pr-10'>
+      <div className='flex gap-2 w-full h-full'>
+          <PartBDivision />
+          <PartBSheet />
+      </div>
+      <PartBInstruction />
     </div>
   )
 }
---
+++ src/app/facilitator/parttime/PartBSheet.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import ExpansionToggle from '../fac_components/ExpansionToggle'
+import Image from 'next/image'
+
+const PartBSheet = () => {
+    const [showInstructions, setShowInstructions] = useState(false)
+
+    const handleInstructions = () => {
+        setShowInstructions(!showInstructions)
+    }
+  return (
+    <div className={`window ${showInstructions ? 'w-full' : 'w-10'} relative`}>
+      {showInstructions && (
+        <div className='h-full w-full flex items-center justify-center'>
+            <Image 
+                src='/DD_ARC_WS_Sheet1.jpg' 
+                alt='Part B Sheet' 
+                width={500} 
+                height={500} 
+                className='border-[0.25px] max-w-full max-h-full object-contain border-[var(--primary-blue)] rounded-md'
+            />
+        </div>
+      )}
+      <ExpansionToggle className='absolute top-2 right-2' handleInstructions={handleInstructions} showInstructions={showInstructions}/>
+    </div>
+  )
+}
+
+export default PartBSheet
